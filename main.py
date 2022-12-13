06e755f89204411e5c28f2e367de8e9073f864f6
santiago rubiano <90368949+santiagorubiano@users.noreply.github.com>
2022-12-12 19:11:34 -0500

    reto porcentaje sur america

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import charts
 
 def run():
   data = read_csv.read_csv('./data.csv')
-#   data = list(filter(lambda item : item['Continent'] == 'South America',data))
+  data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
-#   countries = list(map(lambda x: x['Country'], data))
-#   percentages = list(map(lambda x: x['World Population Percentage'], data))
-#   charts.generate_pie_chart(countries, percentages)
+  countries = list(map(lambda x: x["Country/Territory"], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
   
-  country = input('Type Country => ')
+#   country = input('Type Country => ')
 
-  result = utils.population_by_country(data, country)
+#   result = utils.population_by_country(data, country)
 
-  if len(result) > 0:
-    country = result[0]
-    labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
+#   if len(result) > 0:
+#     country = result[0]
+#     labels, values = utils.get_population(country)
+#     charts.generate_bar_chart(labels, values)
   
 
 if __name__ == '__main__':
